cf5bc0bd83e9d5c174cc8968e7a2d658753a6b16
AtomLabX <olmpy@hotmail.com>
2023-03-12 22:55:57 -0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("I love animals!");
+        System.out.println("Let's check on the animals...");
+        System.out.println("The deer looks fine.");
+        System.out.println("The bat looks happy.");
+        System.out.println("The lion looks healthy.");
     }
 }
