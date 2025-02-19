1f4f887d222c68064079b3cb29a0cdbcb5259792
Adam <olmpy@hotmail.com>
2025-02-18 18:22:06 -0700

    Added camel anime string

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,44 @@
 import java.util.Scanner;
 
-public class Main {
-    Scanner scanner = new Scanner(System.in);
+/**
+ * Zookeper is a program that helps monitor animals at a Zoo.
+ * 
+ * @author Adam 
+ * @version 1.0, Last modifed Feb 18th, 2025
+ */
+
+
+public class Main { // main class
     public static void main(String[] args) {
+        String camel = """
+            Switching on the camera in the camel habitat...
+             ___.-''''-.
+            /___  @    |
+            ',,,,.     |         _.'''''''._
+                 '     |        /           \\
+                 |     \\    _.-'             \\
+                 |      '.-'                  '-.
+                 |                               ',
+                 |                                '',
+                  ',,-,                           ':;
+                       ',,| ;,,                 ,' ;;
+                          ! ; !'',,,',',,,,'!  ;   ;:
+                         : ;  ! !       ! ! ;  ;   :;
+                         ; ;   ! !      ! !  ; ;   ;,
+                        ; ;    ! !     ! !   ; ;
+                        ; ;    ! !    ! !     ; ;
+                       ;,,      !,!   !,!     ;,;
+                       /_I      L_I   L_I     /_I
+            Look at that! Our little camel is sunbathing!
+            """;
+
         System.out.println("I love animals!");
         System.out.println("Let's check on the animals...");
         System.out.println("The deer looks fine.");
         System.out.println("The bat looks happy.");
         System.out.println("The lion looks healthy.");
+
+        System.out.println(camel);
+        
     }
 }
